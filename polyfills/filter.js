6110fac52019d458fc569fb41df9924c57715d90
Shivank12batra <Shivank56batra@gmail.com>
2023-06-11 23:57:39 +0530

    filter method implementation with this keyword

--- polyfills/filter.js
+++ polyfills/filter.js
@@ -1,20 +1,30 @@
-// polyfill for filter
-const arr = [1, 3, 5, 7, 9, 12, 14]
+const numbers = [1, 3, 5, 7, 9, 12, 14]
 
 const filterOddNums = num => num % 2 === 0
 
 const greaterThanTen = num => num > 10
 
-const filter = (arr, fn) => {
-    const newArr = []
-    for (var i=0; i<arr.length; i++) {
-        const element = arr[i]
-        if (fn(element)) newArr.push(element)
+Array.prototype.myFilter = function(func) {
+    let filtered = []
+    console.log(this)
+    for (let i=0; i<this.length; i++) {
+        if (func(this[i], i, this)) {
+            filtered.push(this[i])
+        }
     }
-    return newArr
+    return filtered
 }
 
-const filteredArr = filter(arr, filterOddNums)
-const greaterThanArr = filter(arr, greaterThanTen)
+// const filter = (arr, fn) => {
+//     const newArr = []
+//     for (var i=0; i<arr.length; i++) {
+//         const element = arr[i]
+//         if (fn(element)) newArr.push(element)
+//     }
+//     return newArr
+// }
+
+const filteredArr = numbers.myFilter(filterOddNums)
+const greaterThanArr = numbers.myFilter(greaterThanTen)
 console.log(filteredArr)
 console.log(greaterThanArr)
